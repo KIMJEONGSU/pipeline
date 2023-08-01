75d896b5022349b19c6192bb909a2ccdda38a192
KIMJEONGSU <wjdtn8536@naver.com>
2023-08-01 21:30:13 +0900

    settings 변경

--- mysite/mysite/settings/base.py
+++ mysite/mysite/settings/base.py
@@ -15,7 +15,7 @@ import os, json
 from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -38,7 +38,7 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.34.114.171']
 
 
 # Application definition
